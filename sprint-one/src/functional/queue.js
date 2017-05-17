d3ff1b0941e3fd5397d465b7087e31d4122d2aae
dandersonstack <danieltanderson@berkeley.edu>
2017-05-17 11:27:31 -0700

    competed stack + queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,25 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var currentFront = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[Object.keys(storage).length + currentFront] = value;
   };
 
   someInstance.dequeue = function() {
+    let item = storage[currentFront];
+    delete storage[currentFront];
+    if(item !== undefined) {
+      currentFront++;
+    }
+    return item;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
