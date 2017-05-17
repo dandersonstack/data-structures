a034116cd210794b15be902b8ffaee0c400d427b
dandersonstack <danieltanderson@berkeley.edu>
2017-05-17 16:07:23 -0700

    finished basic hashTable implementation, still need to handle resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,15 +6,31 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  let key = k;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // var value = {[key]:v};
+  // console.log(value);
+  let olderValues = this._storage.get(index);
+  if(this._storage.get(index)) {
+    olderValues[key] = v;
+  } else {
+    olderValues = {[key]:v};
+  }
+  this._storage.set(index, olderValues);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let currBucket = this._storage.get(index);
+  return currBucket[k];
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let currBucket = this._storage.get(index);
+  delete currBucket[k];
+  this._storage.set(index, currBucket);
 };
 
 
