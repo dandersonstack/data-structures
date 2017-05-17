27873525fff8220ed081c847e655062b7a311b7c
dandersonstack <danieltanderson@berkeley.edu>
2017-05-17 16:37:20 -0700

    finished binary tree, need to add hashtable for dynamic sizing + descriptions of runtime

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,57 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(setBSTMethods);
+  bst.value = value;
+  bst.right = null;
+  bst.left = null;
+  return bst
 };
+
+setBSTMethods = {};
+
+
+setBSTMethods.insert = function(value) {
+  if(value < this.value) {
+    if(this.left == null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if(this.right == null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+
+}
+
+setBSTMethods.contains = function(value) {
+  if(this.value === value) {
+    return true;
+  } 
+  else if(value < this.value) {
+    if(this.left == null) {
+      return false;
+    }
+    return this.left.contains(value);
+  }
+  else if (this.right == null) {
+    return false;
+  }
+  return this.right.contains(value);
+}
+
+setBSTMethods.depthFirstLog = function(callback) {
+  callback(this.value);
+  if(this.left) {
+    this.left.depthFirstLog(callback);
+  }
+  if(this.right) {
+    this.right.depthFirstLog(callback);
+  }
+
+}
 
 
 /*
