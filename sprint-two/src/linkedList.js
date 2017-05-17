d773e2abe5df6df6f3ed240e62f17a5fe875f032
dandersonstack <danieltanderson@berkeley.edu>
2017-05-17 14:26:48 -0700

    linkedlist complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = new Node(value);
+    if(list.head === null) {
+      list.head = newTail;
+      list.tail = newTail;
+    }else {
+      list.tail.next = newTail;
+      list.tail = newTail;
+    }
+
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    if(list.head !== null) {
+      list.head = list.head.next;
+    }
+    return oldHead.value;
+
   };
 
   list.contains = function(target) {
+    var pointer = list.head;
+    while(pointer !== null) {
+      if(pointer.value === target) {
+        return true;
+      }
+      pointer = pointer.next;
+    }
+    return false;
   };
 
   return list;
