752a3b0dedb5ce72a5ae6c49e8134555351fa89d
dandersonstack <danieltanderson@berkeley.edu>
2017-05-18 15:26:49 -0700

    added Tree parent functionality

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -16,6 +16,12 @@ describe('tree', function() {
     expect(tree.children[0].value).to.equal(5);
   });
 
+  it('if we add children to the tree it should refrences its parent', function() {
+    tree.addChild(5);
+    tree.children[0].addChild(4);
+    expect(tree.children[0].children[0].parent.value).to.equal(5);
+  });
+
   it('should return true for a value that the tree contains', function() {
     tree.addChild(5);
     expect(tree.contains(5)).to.equal(true);
@@ -39,6 +45,17 @@ describe('tree', function() {
     tree.children[1].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+  });
+
+  it('should correctly remove a child from the parent', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    var tree2 = tree.children[1].removeFromParent();
+    expect(tree.contains(6)).to.equal(false);
+    expect(tree.contains(8)).to.equal(false);
+    expect(tree2.parent).to.equal(null);
   });
 
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,6 +1,7 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.parent = null;
 
   // your code here
   newTree.children = [];  // fix me
@@ -15,11 +16,11 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   var childTree = new Tree(value);
+  childTree.parent = this;
   this.children.push(childTree);
 };
 
 treeMethods.contains = function(target) {
-  //debugger;
   if(this.value === target) {
     return true;
   }
@@ -29,6 +30,22 @@ treeMethods.contains = function(target) {
   }
   return result;
 };
+
+treeMethods.removeFromParent = function() {
+  let foundIndex = null;
+  if(this.parent !== null){
+    for(let index = 0; index < this.parent.children.length; index++) {
+      if(this.parent.children[index].value === this.value) {
+        foundIndex = index;
+      }
+    }
+    if(foundIndex) {
+      this.parent.children.splice(foundIndex,1);
+    }
+    this.parent = null;
+  }
+  return this;
+}
 
 
 
