16772965a25930ee1d039083b0e128c09fc954ac
dandersonstack <danieltanderson@berkeley.edu>
2017-05-20 16:36:28 -0700

    restored binary search tree to its original passing glory and pushed it

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -5,17 +5,13 @@ describe('binarySearchTree', function() {
     binarySearchTree = BinarySearchTree(5);
   });
 
-  it('should have methods named "insert", "contains", and "depthFirstLog', function() {
+  it('should have methods named "insert", "contains", and "depthFirstLog", "breadthFirstLog"', function() {
     expect(binarySearchTree.insert).to.be.a('function');
     expect(binarySearchTree.contains).to.be.a('function');
     expect(binarySearchTree.depthFirstLog).to.be.a('function');
     expect(binarySearchTree.breadthFirstLog).to.be.a('function');
   });
 
-  it('for balancing it should have methods named "isUnbalanced" and "balanceTree"', function() {
-    expect(binarySearchTree.isUnbalanced).to.be.a('function');
-    expect(binarySearchTree.balanceTree).to.be.a('function');
-  });
 
   it('should insert values at the correct location in the tree', function() {
     binarySearchTree.insert(2);
@@ -25,68 +21,6 @@ describe('binarySearchTree', function() {
     expect(binarySearchTree.left.right.value).to.equal(3);
     expect(binarySearchTree.right.left.value).to.equal(6);
   });
-
-  it('should know its parent, and be able to trace its parents to the root', function() {
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(7);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(6);
-    let leaf = binarySearchTree.left.right
-    expect(leaf.value).to.equal(3);
-    expect(leaf.parent.value).to.equal(2);
-    expect(leaf.parent.parent.value).to.equal(5);
-  });
-
-  it('it should calculate the height when it has sub trees on both sides', function() {
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(7);
-    expect(binarySearchTree.height).to.equal(2);
-    expect(binarySearchTree.left.height).to.equal(1);
-    expect(binarySearchTree.right.height).to.equal(1);
-  });
-
-  it('the height of the tree should escalade up', function() {
-    expect(binarySearchTree.height).to.equal(0);
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(7);
-    binarySearchTree.insert(1);
-    expect(binarySearchTree.height).to.equal(3);
-    expect(binarySearchTree.left.height).to.equal(2);
-    expect(binarySearchTree.left.left.height).to.equal(1);
-  });
-
-  it('should balance the binary tree for only right children', function() {
-    var array = [];
-    var func = function(value) { array.push(value); };
-    // debugger;
-    binarySearchTree.insert(7);
-    binarySearchTree.insert(8);
-    binarySearchTree.depthFirstLog(func);
-    expect(array).to.eql([7, 5, 8]);
-  });
-
-  it('should balance the binary tree for only left children', function() {
-    var array = [];
-    var func = function(value) { array.push(value); };
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(1);
-    binarySearchTree.depthFirstLog(func);
-    expect(array).to.eql([2, 1, 5]);
-  });
-
-  it('should return false if the tree is not unBalanced', function() {
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(7);
-    expect(binarySearchTree.isUnbalanced()).to.equal(false);
-    expect(binarySearchTree.left.isUnbalanced()).to.equal(false);
-  });
-
-  // it('should return true if the tree is unBalanced', function() {
-  //   binarySearchTree.insert(2);
-  //   binarySearchTree.insert(1);
-  //   expect(binarySearchTree.isUnbalanced()).to.equal(true);
-  //   expect(binarySearchTree.left.isUnbalanced()).to.equal(false);
-  // });
 
   it('should have a working "contains" method', function() {
     binarySearchTree.insert(2);
